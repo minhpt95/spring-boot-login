68e2a544726956e63d395010b6e67f31319495fd
Pham Tuan Minh <minhpt10595@gmail.com>
2022-05-05 07:12:59 +0700

    add Entity Of Aircraft

--- src/main/java/com/catdev/project/entity/AirportEntity.java
+++ src/main/java/com/catdev/project/entity/AirportEntity.java
@@ -6,15 +6,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 
 @Entity
-@Table(name = "country")
+@Table(name = "airport")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AirportEntity extends DateTimeEntity {
+    @Id
+    @Column(nullable = false,length = 3)
+    private String airportCode;
+
+    @Column
+    private String name;
+
+    @Column
+    private String city;
 }
